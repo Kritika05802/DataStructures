09ed32582a35582b2a035c8bf127a2a83e80e1c1
Kritika Saxena <82437949+Kritika05802@users.noreply.github.com>
2021-05-16 20:46:01 +0530

    Add files via upload

---
+++ file_extns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+filename = input("Input name of the file: ")
+file_extns = filename.split(".")
+print("The extension of the file is: "+ repr(file_extns[-1]))
+
+
+# In[ ]:
+
+
+
+
