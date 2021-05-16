6e70d839867f0c39c1d63967aed83eedd7a281dd
Kritika Saxena <82437949+Kritika05802@users.noreply.github.com>
2021-05-16 20:47:28 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * (radius) * (radius)
+print("The area of the circle with radius "+ str(radius) +" is " + str(area))
+
+
+# In[ ]:
+
+
+
+
